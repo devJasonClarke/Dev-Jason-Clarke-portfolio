4c27b4e6f4fd88d0b0bc8afdf21d9d5b66018501
jettison200 <jettison200@gmail.com>
2022-07-24 18:51:01 -0600

    feat: test getting single document via uid and doc type

--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,16 @@ export default function Home({ page }) {
   console.log(page);
   return (
     <div>
-      <ul>
+      <PrismicText field={page.data.title} />
+
+      {/* <ul>
         {page.map((document) => (
           <div key={document.id}>
             <li>{document.data.title[0].text}</li>
             <PrismicText field={document.data.title} />
           </div>
         ))}
-      </ul>
+      </ul> */}
     </div>
   );
 }
@@ -20,7 +22,8 @@ export default function Home({ page }) {
 export async function getServerSideProps({ previewData }) {
   const client = createClient({ previewData });
 
-  const page = await client.getAllByType("blog_post");
+  const page = await client.getByUID("blog_post", "test");
+/*   const page = await client.getAllByType("blog_post", "test"); */
 
   return {
     props: { page }, // Will be passed to the page component as props
