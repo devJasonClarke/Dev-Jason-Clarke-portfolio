966dccd1593c8b0ef0699127931ca8b37e220e82
jettison200 <jettison200@gmail.com>
2022-07-26 17:26:55 -0600

    feat: removed AOS from _app

--- pages/_app.js
+++ pages/_app.js
@@ -3,18 +3,8 @@ import "../styles/globals.scss";
 import { PrismicProvider } from "@prismicio/react";
 import { PrismicPreview } from "@prismicio/next";
 import { linkResolver, repositoryName } from "../prismicio";
-import AOS from "aos";
-import "aos/dist/aos.css";
-import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
-  useEffect(() => {
-    AOS.init({
-      once: true,
-      offset: 50,
-    });
-  }, []);
-
   return (
     <PrismicProvider
       linkResolver={linkResolver}
