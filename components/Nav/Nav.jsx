f310051b6f15cb5595cfcb0438f3ec93afe2196b
jettison200 <jettison200@gmail.com>
2022-08-02 13:09:29 -0500

    feat: removed motion from logo

--- components/Nav/Nav.jsx
+++ components/Nav/Nav.jsx
@@ -3,19 +3,19 @@ import Image from "next/image";
 import styles from "./Nav.module.scss";
 import MobileMenu from "../MobileMenu/MobileMenu";
 import Link from "next/link";
-import { motion } from "framer-motion";
+
 const Nav = () => {
   return (
     <nav className={styles.nav}>
       <Link href="/">
-        <motion.a
+        <a
          initial={{  opacity: 0 }}
          animate={{ opacity: 1 }}
          transition={{ duration: 0.5 }}
           className={styles.logo}>
           <span>Dev</span>
           <span>Jason</span>
-        </motion.a>
+        </a>
       </Link>
       <div className={styles["nav-links-container"]}>
         <NavLinks delay={0.05} />
