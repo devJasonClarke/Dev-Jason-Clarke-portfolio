1566a32be0a4f06ad42657d7d44f2e869e5e0513
jettison200 <jettison200@gmail.com>
2022-09-26 16:19:38 -0500

    feat: added google tag manager

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,10 +7,35 @@ import Layout from "../components/Layout/Layout";
 import { store } from "../redux/store";
 import { Provider } from "react-redux";
 import Head from "next/head";
+import Script from "next/script";
+import { GTM_ID, pageview } from "../lib/gtm";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    router.events.on("routeChangeComplete", pageview);
+    return () => {
+      router.events.off("routeChangeComplete", pageview);
+    };
+  }, [router.events]);
+
   return (
     <>
+      <Script
+        id="gtag-base"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', '${GTM_ID}');
+          `
+        }}
+      />
       <Head>
         {" "}
         <meta name="application-name" content="Dev Jason" />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import { GTM_ID } from "../lib/gtm";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -22,6 +23,14 @@ class MyDocument extends Document {
           />
         </Head>
         <body>
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            />
+          </noscript>
           <Main />
           <NextScript />
         </body>
