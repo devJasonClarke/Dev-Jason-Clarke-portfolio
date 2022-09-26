223dfec930a391fb28683ae4583cdbf18dfb94ed
jettison200 <jettison200@gmail.com>
2022-09-26 18:17:10 -0500

    feat: updated noscript

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,11 +17,14 @@ export default function MyDocument() {
         />
       </Head>
       <body>
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`
-          }}
-        />
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <Main />
         <NextScript />
       </body>
