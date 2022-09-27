e4491e22083b906878a1506657dc7dc3e21bd6ad
jettison200 <jettison200@gmail.com>
2022-09-26 22:42:18 -0500

    feat: replaced jarallax on regular hero

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -2,13 +2,11 @@ import styles from "./Hero.module.scss";
 import { PrismicRichText } from "@prismicio/react";
 import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
-import dynamic from "next/dynamic";
-import Image from "next/image";
-
-const Jarallax = dynamic(() => import("../Jarallax/Jarallax"), {
-  ssr: false
-});
-
+import {
+  ParallaxBanner,
+  ParallaxBannerLayer,
+  ParallaxProvider
+} from "react-scroll-parallax";
 const Hero = ({ data }) => {
   const [theData, setTheData] = useState();
   // During hydration `useEffect` is called. `window` is available in `useEffect`. In this case because we know we're in the browser checking for window is not needed. If you need to read something from window that is fine.
@@ -17,45 +15,34 @@ const Hero = ({ data }) => {
 
   return (
     <header>
-      <Jarallax
-        speed={0.2}
-        /*    videoSrc="https://youtu.be/S4L8T2kFFck" */
-      >
-        <Image
-          className={`jarallax-img`}
-          src="https://images.unsplash.com/photo-1515704089429-fd06e6668458?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-          alt="img"
-          layout="fill"
-        />
-        <motion.div
-          className={
-            theData?.data?.hero_heading
-              ? styles.container
-              : `${styles.container} ${styles.alt}`
-          }
-          initial={{ opacity: 0, y: 50 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.4 }}
-        >
-          <PrismicRichText
-            field={
-              theData?.data?.hero_heading
-                ? theData?.data?.hero_heading
-                : theData?.data?.title
-            }
+      <ParallaxProvider>
+        <ParallaxBanner>
+          <ParallaxBannerLayer
+            image="https://images.unsplash.com/photo-1515704089429-fd06e6668458?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+            speed={-20}
+            className={styles.background}
           />
-        </motion.div>
-      </Jarallax>
+          <motion.div
+            className={
+              theData?.data?.hero_heading
+                ? styles.container
+                : `${styles.container} ${styles.alt}`
+            }
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.4 }}
+          >
+            <PrismicRichText
+              field={
+                theData?.data?.hero_heading
+                  ? theData?.data?.hero_heading
+                  : theData?.data?.title
+              }
+            />
+          </motion.div>
+        </ParallaxBanner>
+      </ParallaxProvider>
     </header>
-    /*  <header className={styles.header}>
-      <motion.div
-        initial={{ opacity: 0, y: 50 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.4 }}
-      >
-        <PrismicRichText field={theData?.data?.hero_heading} />
-      </motion.div>
-    </header> */
   );
 };
 export default Hero;
