0f2ee5db2a04a365397dba83c1909990c0374fd4
jettison200 <jettison200@gmail.com>
2022-09-29 16:20:15 -0500

    feat: removed unused redux slice

--- redux/store.ts
+++ redux/store.ts
@@ -1,12 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import navLinksReducer from "./nav/navSlice";
-import cmsDataReducer from "./cms/cmsSlice";
 import blogHeroImgReducer from "./blogHeroImg/blogHeroImg";
 import blogPageReducer from "./blogPage/blogPage";
 export const store = configureStore({
   reducer: {
     links: navLinksReducer,
-    cms: cmsDataReducer,
     blogHeroImg: blogHeroImgReducer,
     blogPage: blogPageReducer
   }
