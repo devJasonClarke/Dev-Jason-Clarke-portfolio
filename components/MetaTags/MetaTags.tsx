6d3c3b28570e8881d1ae8a693e5fc36f17df0c3b
jettison200 <jettison200@gmail.com>
2022-09-29 16:04:26 -0500

    feat: removed meta tags from _app.tsx

--- components/MetaTags/MetaTags.tsx
+++ components/MetaTags/MetaTags.tsx
@@ -4,6 +4,48 @@ const MetaTags = ({ title, description, image }) => {
     <Head>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <title>{`${title} | Dev Jason Clarke`}</title>
+
+      <meta name="application-name" content={`${title} | Dev Jason Clarke`} />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+      <meta
+        name="apple-mobile-web-app-title"
+        content={`${title} | Dev Jason Clarke`}
+      />
+      <meta name="format-detection" content="telephone=no" />
+      <meta name="mobile-web-app-capable" content="yes" />
+      <meta name="msapplication-TileColor" content="#000000" />
+      <meta name="msapplication-tap-highlight" content="no" />
+      <meta name="theme-color" content="#000000" />
+      <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
+      <link
+        rel="apple-touch-icon"
+        sizes="152x152"
+        href="/icons/touch-icon-ipad.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/icons/touch-icon-iphone-retina.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="167x167"
+        href="/icons/touch-icon-ipad-retina.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/icons/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/icons/favicon-16x16.png"
+      />
+
       <link
         rel="apple-touch-icon"
         sizes="180x180"
@@ -21,7 +63,7 @@ const MetaTags = ({ title, description, image }) => {
         sizes="16x16"
         href="/favicon-16x16.png"
       />
-      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="manifest" href="/manifest.json" />
       <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ffffff" />
       <meta name="msapplication-TileColor" content="#ffffff" />
       <meta name="theme-color" content="#0c0c0c" />
